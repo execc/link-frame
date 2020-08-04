5e7151cf411884b0988285819f1e35ddaefd9a07
dvasin <dvasin@wavesplatform.com>
2020-08-04 17:00:55 +0300

    First version (DNS Resolution) is done

---
+++ src/bg/background.js
@@ -0,0 +1,128 @@
+const RESOLUTION_TIMEOUT = 10000
+
+const pac = {
+  _scriptStub: function () {
+    var cache = CACHE_HERE;
+  
+    function FindProxyForURL(url, host) {
+      var res = 'DIRECT';
+      var ips = cache[host];
+  
+      if (ips) {
+        var pos = url.indexOf(host);
+        var port;
+  
+        if (pos != -1) {
+          port = (url.substr(pos + host.length).match(/^:(\d+)/) || [])[1];
+        }
+  
+        var https = url.match(/^https:/i);
+        var directive = https ? 'HTTPS ' : 'PROXY ';
+        port = ':' + (port || (https ? 443 : 80));
+        res = directive + ips.join(port + '; ' + directive) + port;
+      }
+  
+      console.log(`FindProxyForURL: url: ${url}, host: ${host}, res: ${res}`)
+      return res;
+    }
+  },
+  buildObject: function (domain, ip) {
+    console.log(`buildObject: ${domain}, ${ip}`)
+    var obj = {};
+    obj[domain] = [ip];
+    return JSON.stringify(obj);
+  },
+}
+
+const handleObBeforeRequest = async details => {
+  const urlhttpreplace = details.url.replace(/\w+?:\/\//, '')
+  const url = urlhttpreplace.replace(/[\\/].*/g, '') // eslint-disable-line no-useless-escape
+  let domainhtml = urlhttpreplace.match(/[\\/].*/g) // eslint-disable-line no-useless-escape
+  let clearTime = null
+  const name = url.replace(/\/$/g, '')
+  if (domainhtml === null) domainhtml = ['']
+
+  console.log(`Resolving data for address: ${name}`)
+  const resolutionResult = await resolve(url.replace('.hns', ''))
+  console.log(`Resolved: ${JSON.stringify(resolutionResult)}`)
+
+  if (resolutionResult.success) {
+    // Found info in decentralized DNS
+
+    if (resolutionResult.kind === 'ip') {
+      const script = pac._scriptStub.toString()
+        .replace(/^.*|.*$/g, '')
+        .replace('CACHE_HERE', pac.buildObject(name, resolutionResult.address));
+
+      console.log(`Generated proxy script: ${script}`);
+
+      const config = {
+        mode: 'pac_script',
+        pacScript: {
+          data: script,
+        },
+      };
+
+      chrome.proxy.settings.set({value: config}, function () {
+        console.log('Set new PAC script, length = ' + script.length); 
+      });
+
+      return null;
+    }
+    if (resolutionResult.kind === 'hash') {
+      
+    }
+    // Continue current request
+
+    return {} 
+  } else {
+    // Continue current request
+
+    return {} 
+  }
+
+  chrome.tabs.getSelected(null, tab => {
+    chrome.tabs.update(tab.id, { url: 'loading.html' })
+
+      clearTime = setTimeout(() => {
+          return chrome.tabs.update(tab.id, { url: '404.html' })
+      }, RESOLUTION_TIMEOUT)
+
+      
+
+      /*
+      resolver.resolve(name, provider).then(ipfsHash => {
+          clearTimeout(clearTime)
+          let url = 'https://ipfs.infura.io/ipfs/' + ipfsHash + domainhtml[0]
+          return fetch(url, { method: 'HEAD' }).then(response => response.status).then(statusCode => {
+              if (statusCode !== 200) return extension.tabs.update(tab.id, { url: '404.html' })
+              extension.tabs.update(tab.id, { url: url })
+          })
+          .catch(err => {
+              url = 'https://ipfs.infura.io/ipfs/' + ipfsHash + domainhtml[0]
+              extension.tabs.update(tab.id, {url: url})
+              return err
+          })
+      })
+      .catch(err => {
+          clearTimeout(clearTime)
+          const url = err === 'unsupport' ? 'unsupport' : 'error'
+          extension.tabs.update(tab.id, {url: `${url}.html?name=${name}`})
+      })
+      */
+  })
+  return { cancel: true }
+}
+
+chrome.webRequest.onBeforeRequest.addListener(
+  handleObBeforeRequest, 
+  {
+    urls: [
+      '*://*.hns/', 
+      '*://*.hns/*'
+    ]
+  }, 
+  [
+    "blocking"
+  ]
+)
---
+++ src/bg/resolver.js
@@ -0,0 +1,118 @@
+const DNS_OVER_HTTPS_RESOLVER = 'https://dns.easychain.tech'
+
+const getResolver = () => {
+    return DNS_OVER_HTTPS_RESOLVER
+}
+
+const fetchDns = async url => {
+    return fetch(url)
+            .then(rs => rs.json())
+            .catch(_ => ({
+                Status: 5
+            }))
+}
+
+const resolveIp = async domain => {
+    const queryUrl = `${getResolver()}/resolve?name=${domain}`
+    const result = await fetchDns(queryUrl);
+    console.log(`resolveIp result: ${JSON.stringify(result)}`)
+    const success = result.Status === 0;
+    if (success) {
+        return result.Answer[0].data
+    }
+}
+
+const resolveHash = async domain => {
+    const queryUrl = `${getResolver()}/resolve?name=${domain}&type=TXT`
+    const result = await fetchDns(queryUrl);
+    console.log(`resolveHash result: ${JSON.stringify(result)}`)
+    const success = result.Status === 0;
+    if (success) {
+        return extractHash(result)
+    }
+}
+
+const extractHash = response => {
+    const hashRecords = response.Answer
+        .map(answer => ({
+            data: decode(answer.data[0].data)
+        }))
+        .map(answer => ({
+            provider: getProvider(answer.data),
+            ...answer
+        }))
+        .filter(answer => answer.provider !== undefined)
+    return hashRecords.length === 0 ? undefined : hashRecords[0]
+}
+
+const resolve = async domain => {
+    console.log(`Called resolve for ${domain}`)
+    const ip = await resolveIp(domain)
+    if (ip) {
+        return {
+            'success': true,
+            'kind': 'ip',
+            'address': ip
+        }
+    } else {
+        const hash = await resolveHash(domain)
+        if (hash) {
+            return {
+                'success': true,
+                'kind': 'hash',
+                'name': hash.provider.name,
+                'hash': hash.data
+            }
+        }
+    }
+    return {
+        'success': false
+    }
+}
+
+const decode = array => {
+    var result = "";
+    for (var i = 0; i < array.length; i++) {
+        result += String.fromCharCode(parseInt(array[i]));
+    }
+    return result;
+}
+
+const getProvider = data => {
+    const providers = PROVIDERS.filter(provider => provider.isProviderHash(data))
+    return providers.length === 0 ? undefined : providers[0]
+}
+
+// PROVIDERS
+const isSkynet = hash => {
+    if (!hash || hash.length === 0) {
+        return false;
+    }
+    return Boolean(hash.match(SKYLINK_HASH_PATTERN));
+}
+
+const PROVIDERS = [
+    {
+        'name': 'skynet',
+        'isProviderHash': isSkynet
+    }
+]
+
+const SKYLINK_HASH_PATTERN = /^[a-zA-Z0-9_-]{46}/;
+// END PROVIDERS
+
+// TEST
+const TEST_DATA = 
+{"Status":0,"TC":false,"RD":true,"RA":false,"AD":false,"CD":false,"Question":[{"type":16,"name":"3chffffff"}],"Answer":[{"type":16,"data":[{"type":"Buffer","data":[118,65,67,103,87,70,65,70,105,71,119,115,103,113,45,118,89,50,109,76,100,112,122,88,98,68,108,102,51,67,48,82,121,76,99,113,95,118,56,100,89,85,79,102,71,103]}],"TTL":3600,"name":"3chffffff"}],"Additional":[]}
+
+const test = () => {
+    console.log(extractHash(TEST_DATA))
+}
+
+const test2 = async () => {
+    const result = await resolve('3chffffff')
+    console.log(JSON.stringify(result))
+}
+
+//test2()
+// END TEST
